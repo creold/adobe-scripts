005a084637fba1bbc009cb5933c98c8e09e8a84e
Sergey Osokin <reg@sergosokin.ru>
2021-02-11 12:34:18 +0500

    Update RenameArtboardAsSize.jsx

--- ai/RenameArtboardAsSize.jsx
+++ ai/RenameArtboardAsSize.jsx
@@ -29,10 +29,9 @@
 //@target illustrator
 
 // Global variables
-var SAVE_NAME = true,
+var SAVE_NAME = true, // Set false to overwrite the full name
+    ROUND_SIZE = false, // Set true to get a round number
     SPLIT = '_';
-
-
 
 function main() {
   if (app.documents.length == 0) {
@@ -41,28 +40,95 @@ function main() {
   }
 
   var doc = app.activeDocument;
-  var width, height;
+  var width, height, size;
 
   for (var i = 0; i < doc.artboards.length; i++) {
-    doc.artboards.setActiveArtboardIndex(i);
     var currArtboard = doc.artboards[i];
-    width = currArtboard.artboardRect[2] - currArtboard.artboardRect[0];
-    height = currArtboard.artboardRect[1] - currArtboard.artboardRect[3];
+    
+    width = convertUnits(currArtboard.artboardRect[2] - currArtboard.artboardRect[0], getDocUnit());
+    height = convertUnits(currArtboard.artboardRect[1] - currArtboard.artboardRect[3], getDocUnit());
+
+    if (ROUND_SIZE) {
+      width = Math.round(width);
+      height = Math.round(height);
+    } else {
+      width = width.toFixed(2);
+      height = height.toFixed(2);
+    }
+
+    size = width + 'x' + height + getDocUnit();
 
     if (SAVE_NAME) {
-      currArtboard.name += SPLIT + Math.round(width) + 'x' + Math.round(height);
+      currArtboard.name += SPLIT + size;
     } else {
-      currArtboard.name = Math.round(width) + 'x' + Math.round(height);
+      currArtboard.name = size;
     }
 
   }
 }
 
-function showError(err) {
-  if (confirm(scriptName + ': an unknown error has occurred.\n' +
-      'Would you like to see more information?', true, 'Unknown Error')) {
-    alert(err + ': on line ' + err.line, 'Script Error', true);
+// Units conversion. Thanks for help Alexander Ladygin (https://github.com/alexander-ladygin)
+function getDocUnit() {
+  var unit = activeDocument.rulerUnits.toString().replace('RulerUnits.', '');
+  if (unit === 'Centimeters') unit = 'cm';
+  else if (unit === 'Millimeters') unit = 'mm';
+  else if (unit === 'Inches') unit = 'in';
+  else if (unit === 'Pixels') unit = 'px';
+  else if (unit === 'Points') unit = 'pt';
+  return unit;
+}
+
+function getUnits(value, def) {
+  try {
+    return 'px,pt,mm,cm,in,pc'.indexOf(value.slice(-2)) > -1 ? value.slice(-2) : def;
+  } catch (e) {}
+};
+
+function convertUnits(value, newUnit) {
+  if (value === undefined) return value;
+  if (newUnit === undefined) newUnit = 'px';
+  if (typeof value === 'number') value = value + 'px';
+  if (typeof value === 'string') {
+    var unit = getUnits(value),
+        val = parseFloat(value);
+    if (unit && !isNaN(val)) {
+      value = val;
+    } else if (!isNaN(val)) {
+      value = val;
+      unit = 'px';
+    }
   }
+
+  if (((unit === 'px') || (unit === 'pt')) && (newUnit === 'mm')) {
+      value = parseFloat(value) / 2.83464566929134;
+  } else if (((unit === 'px') || (unit === 'pt')) && (newUnit === 'cm')) {
+      value = parseFloat(value) / (2.83464566929134 * 10);
+  } else if (((unit === 'px') || (unit === 'pt')) && (newUnit === 'in')) {
+      value = parseFloat(value) / 72;
+  } else if ((unit === 'mm') && ((newUnit === 'px') || (newUnit === 'pt'))) {
+      value = parseFloat(value) * 2.83464566929134;
+  } else if ((unit === 'mm') && (newUnit === 'cm')) {
+      value = parseFloat(value) * 10;
+  } else if ((unit === 'mm') && (newUnit === 'in')) {
+      value = parseFloat(value) / 25.4;
+  } else if ((unit === 'cm') && ((newUnit === 'px') || (bnewUnit === 'pt'))) {
+      value = parseFloat(value) * 2.83464566929134 * 10;
+  } else if ((unit === 'cm') && (newUnit === 'mm')) {
+      value = parseFloat(value) / 10;
+  } else if ((unit === 'cm') && (newUnit === 'in')) {
+      value = parseFloat(value) * 2.54;
+  } else if ((unit === 'in') && ((newUnit === 'px') || (newUnit === 'pt'))) {
+      value = parseFloat(value) * 72;
+  } else if ((unit === 'in') && (newUnit === 'mm')) {
+      value = parseFloat(value) * 25.4;
+  } else if ((unit === 'in') && (newUnit === 'cm')) {
+      value = parseFloat(value) * 25.4;
+  }
+  return parseFloat(value);
+}
+
+function showError(err) {
+  alert(err + ': on line ' + err.line, 'Script Error', true);
 }
 
 try {
